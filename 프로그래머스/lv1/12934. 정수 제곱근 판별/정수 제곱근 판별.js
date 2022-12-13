8e489e4a231bac1c0f1916b6c1f3f04e38ee6b2a
DaeHyun_Woo <113018070+WooDaeHyun@users.noreply.github.com>
2022-12-13 23:52:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    
+    if(Math.ceil(Math.sqrt(n)) ** 2 === n){
+        answer = (Math.sqrt(n) + 1) ** 2
+    } else {
+        answer = -1
+    }
+    
+    return answer;
+}
