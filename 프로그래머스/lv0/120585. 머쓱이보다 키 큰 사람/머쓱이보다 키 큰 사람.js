43645977e4f75ac7933753af11225b73ed6b039b
DaeHyun_Woo <po668312@naver.com>
2023-03-26 23:16:10 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,11 +1,9 @@
 function solution(array, height) {
     var answer = 0;
-    
-    for (let x of array) {
-        if(x > height) {
+    for(let x of array) {
+        if(height < x) {
             answer++;
         }
     }
-    
     return answer;
 }
