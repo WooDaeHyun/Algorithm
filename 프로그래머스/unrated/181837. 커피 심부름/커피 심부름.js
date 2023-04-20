cfcdefa0876fcd9c93a4c53cf4dc9d76dc9417e6
DaeHyunii <po668312@naver.com>
2023-04-21 01:46:03 +0900

    [unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    var answer = 0;
+    
+    for(let x of order) {
+        if(x.includes('americano') || x.includes("anything")) {
+            answer += 4500
+        } else {
+            answer += 5000
+        }
+    }
+    return answer;
+}
