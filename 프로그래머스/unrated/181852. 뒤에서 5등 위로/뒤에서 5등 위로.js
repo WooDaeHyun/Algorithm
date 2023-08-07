34496b0433a2f6306b2e544c62e76a1e7eb00591
Daehyun <po668312@naver.com>
2023-08-07 12:04:47 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.31 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.js
@@ -0,0 +1,9 @@
+function solution(num_list) {
+    var answer = [];
+    
+    num_list.sort((a,b) => a-b);
+    for(let i = 5; i < num_list.length; i++) {
+        answer.push(num_list[i])
+    }
+    return answer;
+}
