470dc747b7fe5e9227438716a56ed26843618623
Daehyun <po668312@naver.com>
2023-08-10 17:56:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    var answer = [];
+    
+    for(let i of my_string) {
+        if(!isNaN(Number(i))) {
+            answer.push(Number(i))
+        }
+    }
+    
+    answer.sort((a,b) => a- b)
+    
+    return answer;
+}
